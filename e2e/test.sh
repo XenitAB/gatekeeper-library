1c8a418ab2b331da1dec766b7a8269f2db410b02
Philip Laine <philip.laine@gmail.com>
2020-12-09 08:40:51 +0100

    Update e2e test

--- e2e/test.sh
+++ e2e/test.sh
@@ -2,5 +2,5 @@
 
 helm repo add gatekeeper https://open-policy-agent.github.io/gatekeeper/charts
 helm upgrade --install gatekeeper gatekeeper/gatekeeper
-helm upgrade --install -f values.yaml templates ../charts/gatekeeper-library-templates/
-helm upgrade --install -f values.yaml constraints ../charts/gatekeeper-library-constraints/
+helm upgrade --install -f values.yaml templates ../charts/gatekeeper-library-templates
+#helm upgrade --install -f values.yaml constraints ../charts/gatekeeper-library-constraints/
