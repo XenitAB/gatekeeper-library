d57c6385fae14c7bae44c42d4b4c6010155a3037
Philip Laine <philip.laine@gmail.com>
2022-02-24 16:27:17 +0100

    Upgrade gatekeeper version in e2e tests

--- e2e/test.sh
+++ e2e/test.sh
@@ -4,6 +4,6 @@ set -e
 kubectl create namespace gatekeeper-system
 helm repo add gatekeeper https://open-policy-agent.github.io/gatekeeper/charts
 helm repo update
-helm upgrade --version "v3.5.1" --install --namespace gatekeeper-system gatekeeper gatekeeper/gatekeeper
+helm upgrade --version "v3.7.1" --install --namespace gatekeeper-system gatekeeper gatekeeper/gatekeeper
 helm upgrade --install --namespace gatekeeper-system -f values.yaml templates ../charts/gatekeeper-library-templates
 helm upgrade --install --namespace gatekeeper-system -f values.yaml constraints ../charts/gatekeeper-library-constraints
